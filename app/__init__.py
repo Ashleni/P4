5dfa581dc82987595a9f1fc735b9dc011ff0b4d0
Ashleni <sroy2005@outlook.com>
2023-05-04 08:58:30 -0400

    running flask app

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+from flask import session           #facilitate sessions!
+from flask import redirect
+import os                           #facilitate key generation
+import sqlite3
+
+
+#______________________
+app = Flask(__name__)    #create Flask object
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
