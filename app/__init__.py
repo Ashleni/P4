49e38af4ce4af674c0d1270cd0938b8a48dfdce4
Kosta Dubovskiy <kostadubovskiy@gmail.com>
2023-05-05 09:10:41 -0400

    index template + rendering

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "No hablo queso"
+    return render_template('index.html')
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
