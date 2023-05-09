1518f532d59d31b25a643be5023adc8d170f043a
Shafiul Haque <shaque30@stuy.edu>
2023-05-09 08:58:19 -0400

    edits

--- app/db.py
+++ app/db.py
@@ -19,3 +19,20 @@ DB_FILE = "p4.db"
 db = sqlite3.connect(DB_FILE, check_same_thread = False)
 c = db.cursor()
 
+c.execute("""CREATE TABLE IF NOT EXISTS usrest(address TEXT, city TEXT, country TEXT, keys TEXT, latitude FLOAT, longitude FLOAT, name TEXT, postalcode INTEGER, province INTEGER, websites TEXT)""")
+
+
+def store_rest_data(stored_data):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("insert into usrest values (?,?,?,?,?,?,?,?,?,?);", stored_data)
+    db.commit()
+    db.close()
+
+def get_rest_everything():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    usrestlist = c.execute("SELECT address, city, country, keys, latitude, longitude, name, postalcode, province, websties from usrest;").fetchall()
+    db.commit()
+    db.close()
+    return usrestlist
