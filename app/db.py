ec17569b6dd3d0248af487f3b73c860172b4c338
Shafiul Haque <shaque30@stuy.edu>
2023-05-09 09:12:26 -0400

    more kaggle

--- app/db.py
+++ app/db.py
@@ -20,6 +20,8 @@ db = sqlite3.connect(DB_FILE, check_same_thread = False)
 c = db.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS usrest(address TEXT, city TEXT, country TEXT, keys TEXT, latitude FLOAT, longitude FLOAT, name TEXT, postalcode INTEGER, province INTEGER, websites TEXT)""")
+c.execute("""CREATE TABLE IF NOT EXISTS nutr(rest TEXT, item TEXT, calories INTEGER, calfat INTEGER, totalfat INTEGER, satfat INTEGER, transfat INTEGER, chol INTEGER, sodium INTEGER, totalcarb INTEGER)""")
+
 
 
 def store_rest_data(stored_data):
@@ -29,10 +31,26 @@ def store_rest_data(stored_data):
     db.commit()
     db.close()
 
+def store_nutr_data(stored_data):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("insert into nutr values (?,?,?,?,?,?,?,?,?,?);", stored_data)
+    db.commit()
+    db.close()
+
 def get_rest_everything():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    usrestlist = c.execute("SELECT address, city, country, keys, latitude, longitude, name, postalcode, province, websties from usrest;").fetchall()
+    usrestlist = c.execute("SELECT address, city, country, keys, latitude, longitude, name, postalcode, province, websites from usrest;").fetchall()
     db.commit()
     db.close()
     return usrestlist
+
+def get_nutr_everything():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    usrestlist = c.execute("SELECT nutr, item, calories, calfat, totalfat, satfat, transfat, chol, sodium, totalcarb from nutr;").fetchall()
+    db.commit()
+    db.close()
+    return usrestlist
+
