95a6b67a768b102db9815d1bac7ec5962ffa4535
Shafiul Haque <shaque30@stuy.edu>
2023-05-10 09:16:43 -0400

    more charts

--- app/db.py
+++ app/db.py
@@ -20,8 +20,8 @@ db = sqlite3.connect(DB_FILE, check_same_thread = False)
 c = db.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS usrest(address TEXT, city TEXT, country TEXT, keys TEXT, latitude FLOAT, longitude FLOAT, name TEXT, postalcode INTEGER, province INTEGER, websites TEXT)""")
-c.execute("""CREATE TABLE IF NOT EXISTS nutr(rest TEXT, item TEXT, calories INTEGER, calfat INTEGER, totalfat INTEGER, satfat INTEGER, transfat INTEGER, chol INTEGER, sodium INTEGER, totalcarb INTEGER)""")
-
+c.execute("""CREATE TABLE IF NOT EXISTS nutr(rest TEXT, item TEXT, calories INTEGER, calfat INTEGER, totalfat INTEGER, satfat INTEGER, transfat INTEGER, chol INTEGER, sodium INTEGER, totalcarb INTEGER, fiber INTEGER, sugar INTEGER, protein INTEGER, vita INTEGER, vitc INTEGER, calcium INTEGER, salad TEXT)""")
+c.execute("""CREATE TABLE IF NOT EXISTS pov(code INTEGER, state TEXT, area TEXT, pov INTEGER, percent FLOAT, povyoung INTEGER, income INTEGER)""")
 
 
 def store_rest_data(stored_data):
@@ -34,7 +34,14 @@ def store_rest_data(stored_data):
 def store_nutr_data(stored_data):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("insert into nutr values (?,?,?,?,?,?,?,?,?,?);", stored_data)
+    c.execute("insert into nutr values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", stored_data)
+    db.commit()
+    db.close()
+
+def store_pov_data(stored_data):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("insert into pov values (?,?,?,?,?,?,?);", stored_data)
     db.commit()
     db.close()
 
@@ -49,8 +56,16 @@ def get_rest_everything():
 def get_nutr_everything():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    usrestlist = c.execute("SELECT nutr, item, calories, calfat, totalfat, satfat, transfat, chol, sodium, totalcarb from nutr;").fetchall()
+    usrestlist = c.execute("SELECT nutr, item, calories, calfat, totalfat, satfat, transfat, chol, sodium, totalcarb, fiber, sugar, protein, vita, vitc, calcium, salad from nutr;").fetchall()
     db.commit()
     db.close()
     return usrestlist
+
+def get_pov_everything():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    povlist = c.execute("SELECT code, state, area, pov, percent, povyoung, income from pov;").fetchall()
+    db.commit()
+    db.close()
+    return povlist
 
